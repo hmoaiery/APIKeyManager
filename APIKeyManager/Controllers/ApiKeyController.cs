8baf1508c36f145e19115f4c75c8978898262547
Hassan Moaiery <h.moaiery@gmail.com>
2025-02-19 16:31:55 +0330

    Added ApiKeyController

---
+++ APIKeyManager/Controllers/ApiKeyController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APIKeyManager.Services;
+
+namespace APIKeyManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiKeyController : ControllerBase
+    {
+        private readonly IApiKeyService _apiKeyService;
+
+        public ApiKeyController(IApiKeyService apiKeyService)
+        {
+            _apiKeyService = apiKeyService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_apiKeyService.GetApiKeys());
+        }
+
+        [HttpPost]
+        public ActionResult Add([FromBody] string apiKey)
+        {
+            _apiKeyService.AddApiKey(apiKey);
+            return Ok("API Key added");
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            _apiKeyService.DeleteApiKey(id);
+            return Ok("API Key deleted");
+        }
+    }
+}
