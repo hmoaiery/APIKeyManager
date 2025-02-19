ec93d339e2f5a5182ee0fb54a2419cf5835af7fc
Hassan Moaiery <h.moaiery@gmail.com>
2025-02-19 16:18:55 +0330

    Added APIKey model - Created APIKey class with necessary properties

---
+++ APIKeyManager/Models/APIKey.cs
@@ -0,0 +1,12 @@
+﻿namespace APIKeyManager.Models
+{
+    public class APIKey
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); // Unique identifier
+        public string Key { get; set; } = string.Empty; // API Key
+        public string Owner { get; set; } = string.Empty; // Owner of the API Key (like Email)
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Creation date
+        public DateTime? Expiration { get; set; } // Optional expiration date
+        public bool IsActive { get; set; } // Is the API Key active?
+    }
+}
